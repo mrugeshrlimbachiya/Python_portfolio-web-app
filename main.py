dbd1b4c6be9dfab5673aaed8ed3691a3bedd033d
mrugesh <mrugeshlimbachiya123@gmail.com>
2024-03-20 04:16:35 +0530

    Create columns, images and description

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Mrugesh Limbachiya")
+    content = """
+    Currently pursuing a major in Computer Engineering with a focus on Cybersecurity from Mumbai University.
+    Actively engaged in the final year of study, honing skills in essential software development tools and languages.
+    Committed to continuous learning and growth, with a keen interest in gaining practical experience in the field.
+    Open to collaboration with industry professionals to further enhance my knowledge and contribute to meaningful projects.
+
+    """
+    st.info(content)
