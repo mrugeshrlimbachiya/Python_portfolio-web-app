1a935dd20f310a466ca22134f84ba84a53e72338
mrugesh <mrugeshlimbachiya123@gmail.com>
2024-03-20 17:23:28 +0530

    Add description on the new columns

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ with col1:
 with col2:
     st.title("Mrugesh Limbachiya")
     content = """
-    Currently pursuing a major in Computer Engineering with a focus on Cybersecurity from Mumbai University.
-    Actively engaged in the final year of study, honing skills in essential software development tools and languages.
-    Committed to continuous learning and growth, with a keen interest in gaining practical experience in the field.
-    Open to collaboration with industry professionals to further enhance my knowledge and contribute to meaningful projects.
-
+    Currently pursuing a major in Computer Engineering with a focus on Cybersecurity 
+    from Mumbai University. Actively engaged in the final year of study, honing 
+    skills in essential software development tools and languages. Committed to 
+    continuous learning and growth, with a keen interest in gaining practical experience
+    in the field. Open to collaboration with industry professionals to further enhance my
+    knowledge and contribute to meaningful projects.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have build
+"""
+st.write(content2)
